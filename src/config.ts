15b4bab5a45e0fcf58b8e7460b2dca02179e3489
Maxim Schiriy <muzikanto@rss.app>
2021-10-09 20:10:40 +0300

    fix

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-const GAS = '200000000000000';
+export const NEAR_GAS = '200000000000000';
 
 function getNearConfig(env: string) {
    const s: {
@@ -9,7 +9,7 @@ function getNearConfig(env: string) {
       nodeUrl: string;
       GAS: string;
    } = {
-      GAS,
+      GAS: NEAR_GAS,
       contractName: '',
       networkId: '',
       helperUrl: '',
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import { useNearContract, useNearWallet, useNear, useNearUser, useNearAccount } from './hooks';
 import NearProvider, { NearEnvironment, NearContext } from './NearProvider';
-import getNearConfig from './config';
+import getNearConfig, { NEAR_GAS } from './config';
 import { getNearError } from './utils';
 
 export {
+   NEAR_GAS,
    useNear,
    useNearWallet,
    useNearUser,
