93aefcc3e78bb425c76338a8be1e2514f6b526d9
Maxim Schiriy <muzikanto@rss.app>
2021-10-18 15:48:39 +0300

    fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,9 @@
 export function getNearError(e: any) {
    try {
+      if (e.kind && typeof e.kind.ExecutionError === 'string') {
+         return e.kind.ExecutionError.split(', filename')[0];
+      }
+
       return String(e).split('panic_msg: "')[1].split('" }')[0];
    } catch (e2) {
       // @ts-ignore
